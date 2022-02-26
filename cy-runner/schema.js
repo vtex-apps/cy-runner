a23257bee0292d1bdf853da68ad17a707bc25fbd
Charles Santos <thyarles@gmail.com>
2022-02-26 03:35:22 -0300

    MODULE Schema validator finished

--- cy-runner/schema.js
+++ cy-runner/schema.js
@@ -13,8 +13,73 @@
 const {get} = require('lodash')
 const qe = require('./utils')
 
+function traverse(result, obj, previousKey) {
+    if (typeof obj == 'object') {
+        for (const key in obj)
+            traverse(result, obj[key], (previousKey || '') + (previousKey ? '.' + key : key))
+    } else {
+        result.push({
+            key: previousKey || '',
+            type: obj,
+        })
+    }
+    return result
+}
+
+function schemaValidator(schema, config, strategy = '') {
+    let skip = []
+    let schemaTraversed = traverse([], schema)
+    const ignore = (key, value) => {
+        let byPass = null
+        key = strategy + key
+        if (/\.enabled/.test(key) && typeof value == "boolean" && !value) {
+            skip.push(key.split('.enabled')[0])
+        }
+        skip.forEach((disabled) => {
+            byPass = !!key.includes(disabled)
+        })
+        return byPass
+    }
+    schemaTraversed.forEach((item) => {
+        let value = get(config, item.key)
+        let crash = false
+        let msg = 'not null'
+        if (ignore(item.key, value)) return
+        if (item.type in [0, 1, 2, 6] && value == null) crash = true
+        switch (item.type) {
+            // String
+            case 0:
+            case 3:
+                msg = 'string'
+                if (value != null && typeof value != 'string') crash = true
+                break
+            // Integer
+            case 1:
+            case 4:
+                msg = 'number'
+                if (value != null && typeof value != 'number') crash = true
+                break
+            // Boolean
+            case 2:
+            case 5:
+                msg = 'boolean'
+                if (value != null && typeof value != 'boolean') crash = true
+                break
+            // // Array
+            // case 7:
+            //     msg = msg + 'array]'
+            //     if (!value.constructor.prototype.hasOwnProperty('push')) crash = true
+            //     break
+            default:
+                break
+        }
+        if (crash) qe.crash(`Parse cy-runner.yml failed [${strategy}${item.key} must be ${msg}]`)
+    })
+    console.log(skip)
+}
+
 exports.validate = (config) => {
-    const SCHEMA = {
+    const BASE_SCHEMA = {
         secretName: 0,
         testConfig: {
             devMode: 2,
@@ -60,85 +125,25 @@ exports.validate = (config) => {
             teardown: {enabled: 2, stopOnFail: 2, path: 0, file: 0},
         }
     }
-    const STRATEGY = {
-        testStrategy: {
-            '*': {
-                enabled: 2,
-                sendDashboard: 2,
-                stopOnFail: 2,
-                hardTries: 1,
-                parallel: 2,
-            }
-        },
+    const STRATEGY_SCHEMA = {
+        enabled: 2,
+        sendDashboard: 2,
+        stopOnFail: 2,
+        hardTries: 1,
+        parallel: 2,
     }
 
-    // Traverse SCHEMA
-    const traverse = (result, obj, previousKey) => {
-        if (typeof obj == 'object') {
-            for (const key in obj)
-                traverse(result, obj[key], (previousKey || '') + (previousKey ? '.' + key : key))
-        } else {
-            result.push({
-                key: previousKey || '',
-                type: obj,
-            })
-        }
-        return result
-    }
+    // Validate base
+    schemaValidator(BASE_SCHEMA, config)
 
-    // Ignore check on disabled items
-    let skip = []
-    const ignore = (key, value) => {
-        let byPass = null
-        if (/\.enabled/.test(key) && !value) {
-            skip.push(key.split('.enabled')[0])
-        }
-        skip.forEach((disabled) => {
-            byPass = !!key.includes(disabled)
-        })
-        return byPass
-    }
-
-    // Validate SCHEMA
-    const SCHEMA_TRAVERSED = traverse([], SCHEMA)
-    SCHEMA_TRAVERSED.forEach((check) => {
-        let value = get(config, check.key)
-        let crash = false
-        let msg = 'not null'
-        if (ignore(check.key, value)) return
-        // Check values can't be null
-        if (check.type in [0, 1, 2, 6] && value == null) crash = true
-        switch (check.type) {
-            // String
-            case 3:
-                msg = 'string'
-                if (value != null && typeof value != 'string') crash = true
-                break
-            // Integer
-            case 4:
-                msg = 'number'
-                if (value != null && typeof value != 'number') crash = true
-                break
-            // Boolean
-            case 5:
-                msg = 'boolean'
-                if (value != null && typeof value != 'boolean') crash = true
-                break
-            // // Array
-            // case 7:
-            //     msg = msg + 'array]'
-            //     if (!value.constructor.prototype.hasOwnProperty('push')) crash = true
-            //     break
-            default:
-                break
-        }
-        if (crash) qe.crash(`Parse cy-runner.yml failed [${check.key} must be ${msg}]`)
+    // Validate test strategies
+    let configSchema = {}
+    Object.entries(config.testStrategy).forEach(entry => {
+        configSchema[entry[0]] = entry[1]
     })
-    console.log(skip)
-
-    // Validate test strategy
-    skip = []
-    const STRATEGY_TRAVERSED = traverse([], STRATEGY)
+    Object.keys(configSchema).forEach(strategy => {
+        schemaValidator(STRATEGY_SCHEMA, configSchema[strategy], `${strategy}.`)
+    })
 
 
     process.exit(0)
