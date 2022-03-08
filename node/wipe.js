ee2b2e5839dfc11781f814b9e13faeec79e5299a
Charles Santos <thyarles@gmail.com>
2022-03-08 09:40:29 -0300

    FIX Change rm files from Teardown to Wipe

--- node/wipe.js
+++ node/wipe.js
@@ -3,11 +3,22 @@ const qe = require('./utils')
 module.exports.wipe = async (config) => {
   const START = qe.tick()
   if (config.workspace.wipe.enabled) {
+    qe.msg('Removing temporary files', 'warn')
+    let tempFiles = ['cypress.env.json', 'cypress.json']
+    tempFiles.forEach(file => {
+      qe.msg(file, true, true)
+      qe.storage(file, 'rm')
+    })
+    config.base.stateFiles.forEach(file => {
+      qe.msg(file, true, true)
+      qe.storage(file, 'rm')
+    })
+    qe.msg('Temporary files removed')
     qe.msg(`Wiping workspace [${config.workspace.name}]`)
     let wipe = config.workspace.wipe
     let stopOnFail = wipe.stopOnFail
     let testPassed = await qe.runCypress(wipe, config)
     if (!testPassed && stopOnFail) await qe.stopOnFail(config, 'wipe')
-  } else qe.msg('[workspace.wipe] is disabled')
+  }
   return qe.toc(START)
 }
