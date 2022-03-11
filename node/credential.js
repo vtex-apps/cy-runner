b2db2549422255c10d22d6880c39f2b3b35f9dfb
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-03-11 23:10:18 +0530

    Fixed credential bug reported sonarcloud

--- node/credential.js
+++ node/credential.js
@@ -18,32 +18,34 @@ exports.credentials = async (config) => {
 
     qe.msg('Requesting admin cookie', true, true)
 
-    const response = await qe.request(axiosConfig)
-
     try {
+      const response = await qe.request(axiosConfig)
+
       if (response.data.authStatus !== 'Success') {
         qe.crash(
           'Failed to get credentials',
           'Check your secrets vtex.apiToken and vtex.apiKey'
         )
       }
+
+      const cookieName = response.data.authCookie.Name
+      const cookieValue = response.data.authCookie.Value
+
+      config.base.vtex.authCookieName = cookieName
+      config.base.vtex.adminAuthCookieValue = cookieValue
+
+      config.base.vtex.baseUrl = qe.generateBaseUrl(config)
+
+      // User cookie
+      qe.msg('Requesting user cookie', true, true)
+      const vtexBin = config.base.vtex.bin
+      const cookieRobot = await qe.toolbelt(vtexBin, 'local token')
+
+      config.base.vtex.userAuthCookieValue = cookieRobot.slice(0, -1)
+      qe.writeEnvJson(config)
     } catch (e) {
       qe.crash('Fail on axios', e)
     }
-
-    const cookieName = response.data.authCookie.Name
-    const cookieValue = response.data.authCookie.Value
-
-    config.base.vtex.authCookieName = cookieName
-    config.base.vtex.adminAuthCookieValue = cookieValue
-    config.base.vtex.baseUrl = qe.generateBaseUrl(config)
-    // User cookie
-    qe.msg('Requesting user cookie', true, true)
-    const vtexBin = config.base.vtex.bin
-    const cookieRobot = await qe.toolbelt(vtexBin, 'local token')
-
-    config.base.vtex.userAuthCookieValue = cookieRobot.slice(0, -1)
-    qe.writeEnvJson(config)
   }
 
   return {
