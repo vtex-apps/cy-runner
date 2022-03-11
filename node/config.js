b1cd3161bef4b872df2426767a043d4c180e8d62
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-03-11 15:54:09 +0530

    Added .eslintrc & Fixedeslint in cli/config/credential

--- node/config.js
+++ node/config.js
@@ -5,14 +5,16 @@ exports.getConfig = async (configFile) => {
   let config = qe.loadYmlConfig(configFile)
   const VTEX_AUTH_PATH = '.myvtex.com/api/vtexid/pub/authentication'
   const VTEX_ACCOUNT = config.base.vtex.account
-  config.base.vtex['authUrl'] = 'https://' + VTEX_ACCOUNT + VTEX_AUTH_PATH
+
+  config.base.vtex.authUrl = `https://${VTEX_ACCOUNT}${VTEX_AUTH_PATH}`
   // Load secrets and parse it
-  let secrets = qe.loadSecrets(config)
+  const secrets = qe.loadSecrets(config)
+
   if (secrets) config = qe.mergeSecrets(config, secrets)
   // Get workspace to run tests
   config.workspace.name = qe.getWorkspaceName(config)
   // Set right vtex cli to be used in further calls
-  config.base.vtex['bin'] = config.base.vtex.deployCli.enabled
+  config.base.vtex.bin = config.base.vtex.deployCli.enabled
     ? 'vtex-e2e'
     : 'vtex'
   // Write cypress.env.json
@@ -21,5 +23,6 @@ exports.getConfig = async (configFile) => {
   qe.writeCypressJson(config)
   // Create empty state files
   qe.createStateFiles(config)
+
   return config
 }
--- node/credential.js
+++ node/credential.js
@@ -2,6 +2,7 @@ const qe = require('./utils')
 
 exports.credentials = async (config) => {
   const START = qe.tick()
+
   if (config.base.secrets.enabled) {
     // Admin cookie
     qe.msgSection('Cookies')
@@ -14,27 +15,39 @@ exports.credentials = async (config) => {
         pass: config.base.vtex.apiToken,
       },
     }
+
     qe.msg('Requesting admin cookie', true, true)
+
     const response = await qe.request(axiosConfig)
+
     try {
-      if (response.data.authStatus !== 'Success')
-        qe.crash('Failed to get credentials',
-          'Check your secrets vtex.apiToken and vtex.apiKey')
+      if (response.data.authStatus !== 'Success') {
+        qe.crash(
+          'Failed to get credentials',
+          'Check your secrets vtex.apiToken and vtex.apiKey'
+        )
+      }
     } catch (e) {
       qe.crash('Fail on axios', e)
     }
-    let cookieName = response.data.authCookie.Name
-    let cookieValue = response.data.authCookie.Value
-    config.base.vtex['authCookieName'] = cookieName
-    config.base.vtex['adminAuthCookieValue'] = cookieValue
+
+    const cookieName = response.data.authCookie.Name
+    const cookieValue = response.data.authCookie.Value
+
+    config.base.vtex.authCookieName = cookieName
+    config.base.vtex.adminAuthCookieValue = cookieValue
+    config.base.vtex.baseUrl = qe.generateBaseUrl(config)
     // User cookie
     qe.msg('Requesting user cookie', true, true)
-    let vtexBin = config.base.vtex.bin
-    let cookieRobot = await qe.toolbelt(vtexBin, 'local token')
-    config.base.vtex['userAuthCookieValue'] = cookieRobot.slice(0, -1)
+    const vtexBin = config.base.vtex.bin
+    const cookieRobot = await qe.toolbelt(vtexBin, 'local token')
+
+    config.base.vtex.userAuthCookieValue = cookieRobot.slice(0, -1)
     qe.writeEnvJson(config)
   }
+
   return {
-    config: config, time: qe.toc(START),
+    config,
+    time: qe.toc(START),
   }
 }
