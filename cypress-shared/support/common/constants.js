1d3420490c0554cdc8ae313cc4316bab7215d349
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-03-17 18:28:09 +0530

    Use CAN instead of Canada

--- cypress-shared/support/common/constants.js
+++ cypress-shared/support/common/constants.js
@@ -18,7 +18,7 @@ export const addressList = {
     fullAddress: '  91 Rue de Strasbourg Saint-Denis 93200',
   },
   invalid: {
-    country: 'Canada',
+    country: 'CAN',
     deliveryScreenAddress: 'Texas Road 8',
     fullAddress: '8 Texas Road',
   },
