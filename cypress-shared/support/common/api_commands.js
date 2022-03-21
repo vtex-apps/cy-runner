189bc0c76f0522b48074ad80adc65f3ea7a52fcb
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-03-21 13:02:10 +0530

    Added orderTaxAPI in api_commands.js + use invoiced directly in refund_apis.js

--- cypress-shared/support/common/api_commands.js
+++ cypress-shared/support/common/api_commands.js
@@ -33,3 +33,34 @@ Cypress.Commands.add('getAPI', (url, headers) => {
     ...FAIL_ON_STATUS_CODE,
   })
 })
+
+// Order Tax API Test Case
+Cypress.Commands.add('orderTaxApi', (requestPayload, tax) => {
+  cy.getVtexItems().then((vtex) => {
+    cy.request({
+      method: 'POST',
+      url: `${vtex.baseUrl}/${
+        Cypress.env('workspace').prefix
+      }/checkout/order-tax`,
+      headers: {
+        Authorization: vtex.authorization,
+        ...VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
+      },
+      ...FAIL_ON_STATUS_CODE,
+      body: requestPayload,
+    }).then((response) => {
+      expect(response.status).to.equal(200)
+
+      const reqIds = requestPayload.items.map(({ id }) => id)
+      const respIds = response.body.itemTaxResponse.map(({ id }) => id)
+
+      expect(reqIds.sort()).to.deep.equal(respIds.sort())
+      let taxFromAPI = 0
+
+      response.body.itemTaxResponse.forEach((item) => {
+        item.taxes.map((obj) => (taxFromAPI += obj.value))
+      })
+      expect(taxFromAPI.toFixed(2)).to.equal(tax.replace('$ ', ''))
+    })
+  })
+})
