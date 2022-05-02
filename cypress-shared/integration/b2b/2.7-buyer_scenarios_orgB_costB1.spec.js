ceed764faf24af9f382e6b89f6a0034c62cffc85
Charles Santos <charles.santos@vtex.com.br>
2022-05-02 07:05:07 -0300

    For Buyer - Buying product is completely disabled now (#27) (#28)
    Co-authored-by: Syed Mujeeb <92449902+Syed-Vtex@users.noreply.github.com>

--- cypress-shared/integration/b2b/2.7-buyer_scenarios_orgB_costB1.spec.js
+++ cypress-shared/integration/b2b/2.7-buyer_scenarios_orgB_costB1.spec.js
@@ -6,16 +6,14 @@ import {
   productShouldNotbeAvailableTestCase,
   verifySession,
 } from '../../support/b2b/common.js'
-import { buyNowProductTestCase } from '../../support/b2b/checkout.js'
 
 describe('Organization B - Cost Center B1 - Buyer Scenario', () => {
   testSetup(false)
 
-  const { product, users, nonAvailableProduct } = b2b.OrganizationB
+  const { users, nonAvailableProduct } = b2b.OrganizationB
 
   loginToStoreFront(users.Buyer1, ROLE_DROP_DOWN.Buyer)
   verifySession(b2b.OrganizationB)
   productShouldNotbeAvailableTestCase(nonAvailableProduct)
-  buyNowProductTestCase(product)
   preserveCookie()
 })
