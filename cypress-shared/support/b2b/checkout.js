cda37f2abc95642f150739bbe105a6cfd1a71262
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-06-19 10:22:32 +0530

    Revert changes in ordertheProduct()

--- cypress-shared/support/b2b/checkout.js
+++ cypress-shared/support/b2b/checkout.js
@@ -90,8 +90,8 @@ export function ordertheProduct(role) {
   it(`Verify ${role} is able to order the product`, () => {
     cy.intercept('**/paymentData').as('paymentData')
     cy.get(`[data-name='${PAYMENT_TERMS.NET30}']`).should('be.visible').click()
-    cy.contains(BUTTON_LABEL.completeOrder).click()
     cy.wait('@paymentData')
+    cy.contains(BUTTON_LABEL.completeOrder).click()
     cy.get(selectors.Search, { timeout: 30000 }).should('be.visible')
   })
 }
