4bfbb7fbaa3bbe2576e3e79ccf6a6c81806b073a
Syed Mujeeb <92449902+Syed-Vtex@users.noreply.github.com>
2022-06-22 16:05:47 +0530

    Fix B2B Organization Name Selector (#47)
    * Fix B2B Organization Name Selector
    
    * In fillCostCenter, scroll and fill + close Modal if opened
    
    * Move deleteOrganization outside  createOrganizationTestCase

--- cypress-shared/integration/b2b/2.1.2-create_&_approve_organizationB.spec.js
+++ cypress-shared/integration/b2b/2.1.2-create_&_approve_organizationB.spec.js
@@ -1,19 +1,25 @@
 import { testSetup } from '../../support/common/support.js'
 import { createOrganizationTestCase } from '../../support/b2b/organization_request.js'
 import b2b from '../../support/b2b/constants.js'
+import { deleteOrganization } from '../../support/b2b/graphql.js'
 
 describe('Create & Approve OrganizationB', () => {
   testSetup(false)
 
+  const { organizationName, costCenter1, users } = b2b.OrganizationB
+
+  deleteOrganization(organizationName, true)
+  deleteOrganization(organizationName)
+
   // Create and approve Organization B
   createOrganizationTestCase(
     {
-      name: b2b.OrganizationB.organizationName,
-      email: b2b.OrganizationB.users.OrganizationAdmin1,
+      name: organizationName,
+      email: users.OrganizationAdmin1,
     },
     {
-      costCenterName: b2b.OrganizationB.costCenter1.name,
-      costCenterAddress: b2b.OrganizationB.costCenter1.addresses[0],
+      costCenterName: costCenter1.name,
+      costCenterAddress: costCenter1.addresses[0],
       approved: true,
     }
   )
