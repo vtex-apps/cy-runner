9a4eea845415312a3833ba2db0338c5189cf09ad
Shashidhar Reddy <shashidhargujjula@gmail.com>
2022-07-20 16:38:07 +0530

    skip 2.6 test file

--- cypress-shared/integration/location-availability/2.6-Verify-location-availability props.spec.js
+++ cypress-shared/integration/location-availability/2.6-Verify-location-availability props.spec.js
@@ -19,24 +19,28 @@ describe('Verify-Location-availability', () => {
     })
   })
 
-  // eslint-disable-next-line jest/expect-expect
-  it('Adding Location & Verify location Availability', updateRetry(1), () => {
-    cy.addNewLocation(country1, postalCode1)
-    // cy.get('div[class*=vtex-modal-layout]').should('not.be.visible')
-    cy.scrollTo(0, 500)
-    cy.getVtexItems().then((vtex) => {
-      cy.intercept('POST', `${vtex.baseUrl}/**`, (req) => {
-        if (req.body.operationName === 'updateOrderFormShipping') {
-          req.continue()
-        }
-      }).as('updateOrderFormShipping')
-      cy.get(selectors.addressContainer).should('be.visible')
-      // cy.get(selectors.AddressCity).contains('Pomona')
-      // cy.get(selectors.AddressZip).contains('91766')
-      cy.get(selectors.Distance).contains('Distance:')
-      cy.wait('@updateOrderFormShipping', { timeout: 20000 })
-    })
-  })
+  // eslint-disable-next-line jest/no-disabled-tests
+  it.skip(
+    'Adding Location & Verify location Availability',
+    updateRetry(1),
+    () => {
+      cy.addNewLocation(country1, postalCode1)
+      // cy.get('div[class*=vtex-modal-layout]').should('not.be.visible')
+      cy.scrollTo(0, 500)
+      cy.getVtexItems().then((vtex) => {
+        cy.intercept('POST', `${vtex.baseUrl}/**`, (req) => {
+          if (req.body.operationName === 'updateOrderFormShipping') {
+            req.continue()
+          }
+        }).as('updateOrderFormShipping')
+        cy.get(selectors.addressContainer).should('be.visible')
+        cy.get(selectors.AddressCity).contains('Pomona')
+        cy.get(selectors.AddressZip).contains('91766')
+        cy.get(selectors.Distance).contains('Distance:')
+        cy.wait('@updateOrderFormShipping', { timeout: 20000 })
+      })
+    }
+  )
 
   // eslint-disable-next-line jest/no-disabled-tests
   it.skip('Search results & product specification', updateRetry(3), () => {
