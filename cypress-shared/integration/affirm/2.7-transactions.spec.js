5d1d491f720f93e1a06cfc6bbd085a99c00f5173
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-08-29 15:15:09 +0530

    Added partialRefund testcase + improvements

--- cypress-shared/integration/affirm/2.7-transactions.spec.js
+++ cypress-shared/integration/affirm/2.7-transactions.spec.js
@@ -1,15 +1,24 @@
 /* eslint-disable jest/expect-expect */
 import { loginViaCookies } from '../../support/common/support.js'
-import { singleProduct } from '../../support/affirm/outputvalidation'
-import { verifyTransactionPaymentsAPITestCase } from '../../support/common/testcase.js'
-import { getTestVariables } from '../../support/affirm/affirm'
+import {
+  singleProduct,
+  multiProduct,
+} from '../../support/affirm/outputvalidation'
+import {
+  verifyTransactionPaymentsAPITestCase,
+  getTestVariables,
+} from '../../support/common/testcase.js'
 import { verifyTransactionInAffirm } from '../../support/affirm/api_testcase.js'
 
 describe(`Transaction Scenarios`, () => {
   loginViaCookies()
 
   const singleProductEnvs = getTestVariables(singleProduct.prefix)
+  const multiProductEnvs = getTestVariables(multiProduct.prefix)
 
-  verifyTransactionPaymentsAPITestCase(singleProductEnvs)
+  verifyTransactionPaymentsAPITestCase(singleProduct.singleProductEnvs)
   verifyTransactionInAffirm(singleProduct, singleProductEnvs)
+
+  verifyTransactionPaymentsAPITestCase(multiProduct, multiProductEnvs)
+  verifyTransactionInAffirm(multiProduct, multiProductEnvs)
 })
--- cypress-shared/support/affirm/api_testcase.js
+++ cypress-shared/support/affirm/api_testcase.js
@@ -1,11 +1,13 @@
 import { FAIL_ON_STATUS_CODE } from '../common/constants.js'
+import { updateRetry } from '../common/support.js'
 
 export function verifyTransactionInAffirm(
-  { prefix, totalProductPrice },
+  { prefix, totalProductPrice, refundType, refundedAmount },
   { paymentTidEnv, orderIdEnv },
-  refund = false
+  refund = ''
 ) {
-  it(`For ${prefix} - Verify Affirm Transaction`, () => {
+  it(`In ${prefix} - Verify Affirm Transaction`, updateRetry(6), () => {
+    cy.addDelayBetweenRetries(4000)
     cy.getOrderItems().then((order) => {
       cy.getVtexItems().then((vtex) => {
         cy.request({
@@ -17,7 +19,9 @@ export function verifyTransactionInAffirm(
           ...FAIL_ON_STATUS_CODE,
         }).then((response) => {
           expect(response.status).to.equal(200)
-          expect(response.body.status).to.equal('captured')
+          expect(response.body.status).to.equal(
+            refund ? refundType : 'captured'
+          )
           // Affirm returns orderId like this 1256770739305
           // So, In 1256770739305-01 use split by - and fetch 1256770739305
           expect(response.body.order_id).to.equal(
@@ -26,27 +30,13 @@ export function verifyTransactionInAffirm(
           // Affirm returns amount like this 108500
           // So, In 1085.00 remove . from total amount
           expect(response.body.amount).to.equal(
-            parseInt(totalProductPrice.replace('.', ''), 10)
+            parseInt(totalProductPrice.replace('.', '').replace(',', ''), 10)
           )
           expect(response.body.id).to.equal(order[paymentTidEnv])
-          refund && expect(response.body.amount_refunded).to.equal('')
+          refund &&
+            expect(response.body.amount_refunded).to.equal(refundedAmount)
         })
       })
     })
   })
 }
-
-// {
-//   "status": "captured",
-//   "amount_refunded": 0,
-//   "provider_id": 1,
-//   "created": "2022-08-25T13:12:57Z",
-//   "order_id": "1256770739305",
-//   "checkout_id": "190PHRCC9I6IGQR3",
-//   "currency": "USD",
-//   "amount": 108500,
-//   "events": [],
-//   "remove_tax": false,
-//   "authorization_expiration": "2022-09-24T13:13:14Z",
-//   "id": "H3Y6-B7Y3"
-// }
