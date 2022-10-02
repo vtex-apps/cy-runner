2b942cffb9109a25dd116625f72b13ac85fe5935
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-10-02 21:36:59 +0530

    Added b2b bindings testcase

--- cypress-shared/support/b2b/graphql.js
+++ cypress-shared/support/b2b/graphql.js
@@ -2,9 +2,12 @@ import { FAIL_ON_STATUS_CODE, VTEX_AUTH_HEADER } from '../common/constants.js'
 import { updateRetry } from '../common/support.js'
 
 // Define constants
+const config = Cypress.env()
 const APP_NAME = 'vtex.b2b-organizations-graphql'
 const APP_VERSION = '*.x'
 const APP = `${APP_NAME}@${APP_VERSION}`
+const { vtex } = config.base
+const CUSTOM_URL = `${vtex.baseUrl}/_v/private/admin-graphql-ide/v0/${APP}`
 
 export function deleteOrganization(search, organizationRequest = false) {
   // Default is organization, if organization request is true then delete organization request
@@ -14,56 +17,144 @@ export function deleteOrganization(search, organizationRequest = false) {
     `Deleting Organizations${func} which we created in this workspace ${search}`,
     updateRetry(2),
     () => {
-      cy.getVtexItems().then((vtex) => {
-        const CUSTOM_URL = `${vtex.baseUrl}/_v/private/admin-graphql-ide/v0/${APP}`
+      const GRAPHQL_DELETE_MUTATION =
+        'mutation' +
+        '($id: ID!)' +
+        `{deleteOrganization${func}(id: $id){status}}`
 
-        const GRAPHQL_DELETE_MUTATION =
-          'mutation' +
-          '($id: ID!)' +
-          `{deleteOrganization${func}(id: $id){status}}`
+      const queryName = `getOrganization${func}s`
 
-        const queryName = `getOrganization${func}s`
+      const GRAPHQL_SEARCH_QUERY =
+        'query' +
+        '($search: String!)' +
+        `{${queryName}(search: $search){data{id,name}}}`
 
-        const GRAPHQL_SEARCH_QUERY =
-          'query' +
-          '($search: String!)' +
-          `{${queryName}(search: $search){data{id,name}}}`
-
-        cy.request({
-          method: 'POST',
-          url: CUSTOM_URL,
-          body: {
-            query: GRAPHQL_SEARCH_QUERY,
-            variables: {
-              search,
-            },
+      cy.request({
+        method: 'POST',
+        url: CUSTOM_URL,
+        body: {
+          query: GRAPHQL_SEARCH_QUERY,
+          variables: {
+            search,
           },
-          headers: VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
-          ...FAIL_ON_STATUS_CODE,
-        }).then((response) => {
-          expect(response.status).to.equal(200)
-          expect(response.body).to.not.have.own.property('errors')
-          for (const { id } of response.body.data[queryName].data) {
-            cy.request({
-              method: 'POST',
-              url: CUSTOM_URL,
-              body: {
-                query: GRAPHQL_DELETE_MUTATION,
-                variables: {
-                  id,
-                },
+        },
+        headers: VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
+        ...FAIL_ON_STATUS_CODE,
+      }).then((response) => {
+        expect(response.status).to.equal(200)
+        expect(response.body).to.not.have.own.property('errors')
+        for (const { id } of response.body.data[queryName].data) {
+          cy.request({
+            method: 'POST',
+            url: CUSTOM_URL,
+            body: {
+              query: GRAPHQL_DELETE_MUTATION,
+              variables: {
+                id,
               },
-              headers: VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
-              ...FAIL_ON_STATUS_CODE,
-            }).then((response2) => {
-              expect(response2.status).to.equal(200)
-              expect(
-                response2.body.data[`deleteOrganization${func}`].status
-              ).to.equal('success')
-            })
-          }
-        })
+            },
+            headers: VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
+            ...FAIL_ON_STATUS_CODE,
+          }).then((response2) => {
+            expect(response2.status).to.equal(200)
+            expect(
+              response2.body.data[`deleteOrganization${func}`].status
+            ).to.equal('success')
+          })
+        }
       })
     }
   )
+}
+
+export function verifySalesChannel(binding = true) {
+  const salesChannel = binding ? 'Principal' : 'Dolar'
+
+  it(`Verify Sales Channel having ${salesChannel}`, updateRetry(2), () => {
+    cy.addDelayBetweenRetries(2000)
+    const GRAPHQL_GET_SALES_CHANNEL_QUERY = 'query' + `{getSalesChannels{name}}`
+
+    cy.request({
+      method: 'POST',
+      url: CUSTOM_URL,
+      body: {
+        query: GRAPHQL_GET_SALES_CHANNEL_QUERY,
+      },
+      headers: VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
+      ...FAIL_ON_STATUS_CODE,
+    }).then(({ status, body }) => {
+      expect(status).to.equal(200)
+      expect(body.data.getSalesChannels.length).equal(1)
+      expect(body.data.getSalesChannels[0].name).equal(salesChannel)
+    })
+  })
+}
+
+export function verifyBindings(email, binding) {
+  it(`Verify Bindings with this emailId - ${email}`, updateRetry(3), () => {
+    cy.addDelayBetweenRetries(2000)
+    const GRAPHQL_GET_SALES_CHANNEL_QUERY =
+      'query($email:String!){' + `getBinding(email:$email)}`
+
+    cy.request({
+      method: 'POST',
+      url: CUSTOM_URL,
+      body: {
+        query: GRAPHQL_GET_SALES_CHANNEL_QUERY,
+        variables: { email },
+      },
+      headers: VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
+      ...FAIL_ON_STATUS_CODE,
+    }).then(({ status, body }) => {
+      expect(status).to.equal(200)
+      expect(body.data.getBinding).equal(binding)
+    })
+  })
+}
+
+function bindingsMutation(payload) {
+  const GRAPHQL_GET_SALES_CHANNEL_QUERY =
+    'mutation($channels: [SalesChannelsInput]!){' +
+    `saveSalesChannels(channels: $channels){` +
+    `id,status,message}}`
+
+  cy.request({
+    method: 'POST',
+    url: CUSTOM_URL,
+    body: {
+      query: GRAPHQL_GET_SALES_CHANNEL_QUERY,
+      variables: payload,
+    },
+    headers: VTEX_AUTH_HEADER(vtex.apiKey, vtex.apiToken),
+    ...FAIL_ON_STATUS_CODE,
+  }).then(({ status, body }) => {
+    expect(status).to.equal(200)
+    expect(body.data.saveSalesChannels.status).equal('success')
+  })
+}
+
+export function addBindings() {
+  it(`Add Bindings`, () => {
+    bindingsMutation({
+      channels: [
+        {
+          id: '1',
+          name: 'Principal',
+        },
+      ],
+    })
+  })
+}
+
+export function removeBindings() {
+  it(`Remove Bindings`, () => {
+    bindingsMutation({
+      channels: [
+        {
+          id: 2,
+          name: 'Dolar',
+        },
+      ],
+    })
+  })
 }
