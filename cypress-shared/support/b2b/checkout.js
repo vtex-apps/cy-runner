27643b2f7b37e8253c99ff946ac10c415dd632e7
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-10-19 15:46:49 +0530

    Updated ordertheProduct()

--- cypress-shared/support/b2b/checkout.js
+++ cypress-shared/support/b2b/checkout.js
@@ -91,12 +91,10 @@ export function verifyPayment(promissory = true) {
 export function ordertheProduct(role) {
   it(`Verify ${role} is able to order the product`, () => {
     cy.intercept('**/paymentData').as('paymentData')
+    cy.get(`[data-name='${PAYMENT_TERMS.NET30}']`).should('be.visible').click()
+    cy.wait('@paymentData')
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(10000)
-    cy.get(`[data-name='${PAYMENT_TERMS.NET30}']`)
-      .should('be.visible')
-      .click({ force: true })
-    cy.wait('@paymentData')
     cy.contains(BUTTON_LABEL.completeOrder)
       .should('be.visible')
       .click({ force: true })
