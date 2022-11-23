80e13681dfc91035131e3814f47a9b20cdbbdbbc
suman123 <sumanrajravi@bitcot.com>
2022-11-23 17:36:18 +0530

    moved commands.js

--- cypress-shared/support/commands.js
+++ cypress-shared/support/commands.js
@@ -290,3 +290,29 @@ Cypress.Commands.add('organizationShouldShowInProfile', () => {
   visitOrganizationPage()
   cy.get(selectors.MyOrganization, { timeout: 25000 }).should('be.visible')
 })
+
+const fedexJson = '.fedexPayload.json'
+
+Cypress.Commands.add('writeAppSettingstoJSON', (data) => {
+  cy.writeFile(fedexJson, data)
+})
+
+Cypress.Commands.add('hideSla', (hide) => {
+  cy.readFile('.fedexPayload.json').then((items) => {
+    const { slaSettings } = items.data.getAppSettings
+
+    for (const ship in slaSettings) {
+      slaSettings[ship].hidden = hide
+    }
+
+    return slaSettings
+  })
+})
+
+Cypress.Commands.add('readAppSettingsFromJSON', () => {
+  cy.readFile('.fedexPayload.json').then((items) => {
+    const { slaSettings } = items.data.getAppSettings
+
+    return slaSettings
+  })
+})
