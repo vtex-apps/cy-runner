94d006dbdc3c15e8aea7210ffd51dcc327df0135
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-11-24 18:43:14 +0530

    Disable updateShipping in 1.3 and updated changelog

--- cypress-shared/integration/quick-order/1.3-upload_csv.spec.js
+++ cypress-shared/integration/quick-order/1.3-upload_csv.spec.js
@@ -15,32 +15,11 @@ describe('Quickorder - SkuCode Quantity testcase', () => {
   quickOrderByXLS(false)
 
   it(
-    `In SkuCode Quantity testcase - Updating Shipping Information`,
-    updateRetry(4),
-    () => {
-      // Update Shipping Section
-      cy.updateShippingInformation({
-        postalCode: '33180',
-        phoneNumber: '(312) 310 3249',
-        timeout: 10000,
-      })
-    }
-  )
-
-  it(
     `In SkuCode Quantity testcase - Verify quantity by price`,
     updateRetry(4),
     () => {
       cy.get('.quantity.badge').first().should('have.text', '2')
       cy.get('.quantity.badge').last().should('have.text', '50')
-    }
-  )
-
-  it(
-    'In SkuCode Quantity testcase - Ordering the product',
-    updateRetry(2),
-    () => {
-      cy.orderProduct()
     }
   )
 
