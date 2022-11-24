70bbb0cc82822f9464965d15a17f8d9bc08d0881
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-11-24 18:31:51 +0530

    Drop shipping info from 1.2

--- cypress-shared/integration/quick-order/1.2-skucode_quantity.spec.js
+++ cypress-shared/integration/quick-order/1.2-skucode_quantity.spec.js
@@ -11,31 +11,10 @@ describe('Quickorder - SkuCode Quantity testcase', () => {
   quickOrderBySkuAndQuantityTestCase1('user', null)
 
   it(
-    `In SkuCode Quantity testcase - Updating Shipping Information`,
-    updateRetry(3),
-    () => {
-      // Update Shipping Section
-      cy.updateShippingInformation({
-        postalCode: '33180',
-        phoneNumber: '(312) 310 3249',
-        timeout: 10000,
-      })
-    }
-  )
-
-  it(
     `In SkuCode Quantity testcase - Verify quantity by price`,
     updateRetry(4),
     () => {
       cy.get('.quantity.badge').first().should('have.text', '2')
-    }
-  )
-
-  it(
-    'In SkuCode Quantity testcase - Ordering the product',
-    updateRetry(2),
-    () => {
-      cy.orderProduct()
     }
   )
 
