7502afe450e5b9ca5ad341efdb628db92fbbe0f7
Syed-Vtex <syed.mujeeb@vtex.com.br>
2022-11-25 17:21:19 +0530

    Fix app name in graphql_apps.js

--- cypress-shared/support/fedex-shipping/graphql_apps.js
+++ cypress-shared/support/fedex-shipping/graphql_apps.js
@@ -1,5 +1,5 @@
 export default {
-  FEDEX_SHIPPING_APP: 'vtexus.fedex-shiping',
+  FEDEX_SHIPPING_APP: 'vtexus.fedex-shipping',
   INVENTORY_GRAPHQL_APP: 'vtex.inventory-graphql',
   LOGISTICS_CARRIER_GRAPHQL_APP: 'vtex.logistics-carrier-graphql',
 }
