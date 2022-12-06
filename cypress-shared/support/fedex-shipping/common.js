8d6330403b4a11889181db76ac4e133c0f3fc1c4
Shashidhar Reddy <shashidhargujjula@gmail.com>
2022-12-06 20:24:27 +0530

    fix

--- cypress-shared/support/fedex-shipping/common.js
+++ cypress-shared/support/fedex-shipping/common.js
@@ -1,9 +1,9 @@
 import {
-  graphql,
   saveAppSetting,
   validateSaveAppSettingResponse,
 } from './graphql_testcase'
 import { FEDEX_SHIPPING_APP } from './graphql_apps.js'
+import { graphql } from '../common/graphql_utils'
 
 export function updateSLASettings(appSetting, rate = 0, percentage = 0) {
   cy.getAppSettingstoJSON().then((items) => {
