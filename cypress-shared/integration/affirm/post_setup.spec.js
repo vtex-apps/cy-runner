ef020f5253ef6e49f81087db0b70ec26fc595161
Syed-Vtex <syed.mujeeb@vtex.com.br>
2023-01-09 16:05:09 +0530

    Added affirm promo tests

--- cypress-shared/integration/affirm/post_setup.spec.js
+++ cypress-shared/integration/affirm/post_setup.spec.js
@@ -1,9 +1,27 @@
-import { loginViaCookies } from '../../support/common/support.js'
+import {
+  loginViaCookies,
+  preserveCookie,
+  scroll,
+  updateRetry,
+} from '../../support/common/support.js'
 import {
   setWorkspaceAndGatewayAffiliations,
   startPaymentE2ETests,
   syncCheckoutUICustom,
 } from '../../support/common/testcase.js'
+import selectors from '../../support/common/selectors'
+
+function verifyAffirmPromotion() {
+  cy.get(selectors.AffirmPromoComponents, { timeout: 20000 }).should(
+    'be.visible'
+  )
+  cy.get(selectors.AffirmModal).should('be.visible').first().click()
+  cy.get(selectors.AffirmIFrame).should('be.visible')
+  cy.getIframeBody(selectors.AffirmIFrame)
+    .find(selectors.AffirmClosePopup)
+    .should('be.visible')
+    .click()
+}
 
 describe('Setting up affirm in dynamic environment', () => {
   loginViaCookies()
@@ -11,4 +29,33 @@ describe('Setting up affirm in dynamic environment', () => {
   startPaymentE2ETests()
   setWorkspaceAndGatewayAffiliations()
   syncCheckoutUICustom()
+
+  it('Set public token in affirm settigs', () => {
+    cy.getVtexItems().then((vtex) => {
+      cy.visit('/admin/apps/vtex.affirm-payment/setup/')
+      cy.getIframeBody(selectors.AdminConfigurationIFrame)
+        .find(selectors.AffirmAdminPublicKey)
+        .should('be.visible')
+        .clear()
+        .type(vtex.publicKeyForPromotionalComponents)
+      cy.getIframeBody(selectors.AdminConfigurationIFrame)
+        .find(selectors.AffirmSubmitConfiguration)
+        .should('be.visible')
+        .click()
+    })
+  })
+
+  it('Verify promotions get displayed in storefront', updateRetry(2), () => {
+    cy.visit('/')
+    cy.get(selectors.ProfileLabel, { timeout: 20000 })
+      .should('be.visible')
+      .should('have.contain', `Hello,`)
+    scroll()
+    verifyAffirmPromotion()
+    cy.url().should('include', '/p')
+    cy.get('div[class*=shippingContainer]').should('be.visible')
+    verifyAffirmPromotion()
+  })
+
+  preserveCookie()
 })
